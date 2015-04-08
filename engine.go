a0b3a33692170f9d68bb207310116ea2c23e4340
yl.ecs <yl.ecs>
2015-04-08 14:38:16 +0800

    add startup script

--- engine.go
+++ engine.go
@@ -1,53 +1,53 @@
 package dockerMan
 
 import (
-    "crypto/tls"
-    "fmt"
-    "net"
-    "net/http"
-    "time"
-    "github.com/yleemj/dockerMan/app/cluster"
+	"crypto/tls"
+	"fmt"
+	"github.com/yleemj/dockerMan/app/cluster"
+	"net"
+	"net/http"
+	"time"
 )
 
 const (
-    httpTimeout = time.Duration(1 * time.Second)
+	httpTimeout = time.Duration(1 * time.Second)
 )
 
 type (
-
-    Engine struct {
-        ID             string          `json:"id,omitempty" gorethink:"id,omitempty"`
-        Engine         *cluster.Engine  `json:"engine,omitempty" gorethink:"engine,omitempty"`
-        DockerVersion  string          `json:"docker_version,omitempty"`
-    }
+	Engine struct {
+		ID            string          `json:"id,omitempty" gorethink:"id,omitempty"`
+		Engine        *cluster.Engine `json:"engine,omitempty" gorethink:"engine,omitempty"`
+		DockerVersion string          `json:"docker_version,omitempty"`
+	}
 )
 
 func dialTimeout(network, addr string) (net.Conn, error) {
-    return net.DialTimeout(network, addr, httpTimeout)
+	//fmt.Printf("network: %s, addr: %s\n", network, addr)
+	return net.DialTimeout(network, addr, httpTimeout)
 }
 
-
 func (e *Engine) Ping() (int, error) {
-    status := 0
-    addr := e.Engine.Addr
-    tlsConfig := &tls.Config{}
+	status := 0
+	addr := e.Engine.Addr
+	tlsConfig := &tls.Config{}
 
-    transport := http.Transport{
-        Dial:            dialTimeout,
-        TLSClientConfig: tlsConfig,
-    }
+	transport := http.Transport{
+		Dial:            dialTimeout,
+		TLSClientConfig: tlsConfig,
+	}
 
-    client := http.Client{
-        Transport: &transport,
-    }
+	client := http.Client{
+		Transport: &transport,
+	}
 
-    uri := fmt.Sprintf("%s/_ping", addr)
-    resp, err := client.Get(uri)
-    if err != nil {
-        return 0, err
-    } else {
-        defer resp.Body.Close()
-        status = resp.StatusCode
-    }
-    return status, nil
+	//fmt.Printf("client config: %v", *tlsConfig)
+	uri := fmt.Sprintf("%s/_ping", addr)
+	resp, err := client.Get(uri)
+	if err != nil {
+		return 0, err
+	} else {
+		defer resp.Body.Close()
+		status = resp.StatusCode
+	}
+	return status, nil
 }
